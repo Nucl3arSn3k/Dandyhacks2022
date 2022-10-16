7f4279afc3ab447393f9b646f65a67f91f6b33aa
Tormenator1 <shredderofwheat@gmail.com>
2022-10-16 10:08:17 -0400

    Docker changes made
    This should work for compiling the webservice once finished

--- flask-app/app.py
+++ flask-app/app.py
@@ -1,7 +1,14 @@
+import os
 from flask import Flask
-app = Flask(__name__, static_folder='../react-app/build', static_url_path='/')
-import requests 
+
+app = Flask(__name__, static_folder="../react-app/build", static_url_path="/")
+import requests
+
 # Serves react app files
-@app.route('/')
+@app.route("/")
 def index():
-    return app.send_static_file('index.html')
+    return app.send_static_file("index.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
